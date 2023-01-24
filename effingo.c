e29ed0d197165ee69802db57029ddaa2933c934d
Brice WEBER KLIPFEL <b.cicada@protonmail.com>
2023-01-24 12:14:13 +0000

    Capacity to copying

--- effingo.c
+++ effingo.c
@@ -10,176 +10,47 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-#include <getopt.h>
+int main(int argc, char *argv[]) {
+    int fd1, fd2;
+    char buffer[1024];
+    ssize_t nread;
 
-#define STDOUT 1
-#define STDERR 2
-
-#define MAX_PATH_LENGTH 4096
-
-#define USAGE_SYNTAX "[OPTIONS] -i INPUT -o OUTPUT"
-#define USAGE_PARAMS "OPTIONS:\n\
-  -i, --input  INPUT_FILE  : input file\n\
-  -o, --output OUTPUT_FILE : output file\n\
-***\n\
-  -v, --verbose : enable *verbose* mode\n\
-  -h, --help    : display this help\n\
-"
-
-/**
- * Procedure which displays binary usage
- * by printing on stdout all available options
- *
- * \return void
- */
-void print_usage(char *bin_name)
-{
-  dprintf(1, "USAGE: %s %s\n\n%s\n", bin_name, USAGE_SYNTAX, USAGE_PARAMS);
-}
-
-/**
- * Procedure checks if variable must be free
- * (check: ptr != NULL)
- *
- * \param void* to_free pointer to an allocated mem
- * \see man 3 free
- * \return void
- */
-void free_if_needed(void *to_free)
-{
-  if (to_free != NULL)
-    free(to_free);
-}
-
-/**
- *
- * \see man 3 strndup
- * \see man 3 perror
- * \return
- */
-char *dup_optarg_str()
-{
-  char *str = NULL;
-
-  if (optarg != NULL)
-  {
-    str = strndup(optarg, MAX_PATH_LENGTH);
-
-    // Checking if ERRNO is set
-    if (str == NULL)
-      perror(strerror(errno));
-  }
-
-  return str;
-}
-
-/**
- * Binary options declaration
- * (must end with {0,0,0,0})
- *
- * \see man 3 getopt_long or getopt
- * \see struct option definition
- */
-static struct option binary_opts[] =
-    {
-        {"help", no_argument, 0, 'h'},
-        {"verbose", no_argument, 0, 'v'},
-        {"input", required_argument, 0, 'i'},
-        {"output", required_argument, 0, 'o'},
-        {0, 0, 0, 0}};
-
-/**
- * Binary options string
- * (linked to optionn declaration)
- *
- * \see man 3 getopt_long or getopt
- */
-const char *binary_optstr = "hvi:o:";
-
-/**
- * Binary main loop
- *
- * \return 1 if it exit successfully
- */
-int main(int argc, char **argv)
-{
-  /**
-   * Binary variables
-   * (could be defined in a structure)
-   */
-  short int is_verbose_mode = 0;
-  char *bin_input_param = NULL;
-  char *bin_output_param = NULL;
-
-  // Parsing options
-  int opt = -1;
-  int opt_idx = -1;
-
-  while ((opt = getopt_long(argc, argv, binary_optstr, binary_opts, &opt_idx)) != -1)
-  {
-    switch (opt)
-    {
-    case 'i':
-      // input param
-      if (optarg)
-      {
-        bin_input_param = dup_optarg_str();
-      }
-      break;
-    case 'o':
-      // output param
-      if (optarg)
-      {
-        bin_output_param = dup_optarg_str();
-      }
-      break;
-    case 'v':
-      // verbose mode
-      is_verbose_mode = 1;
-      break;
-    case 'h':
-      print_usage(argv[0]);
-
-      free_if_needed(bin_input_param);
-      free_if_needed(bin_output_param);
-
-      exit(EXIT_SUCCESS);
-    default:
-      break;
+    if (argc < 3) {
+        printf("Usage: %s file1 file2\n", argv[0]);
+        exit(1);
     }
-  }
 
-  /**
-   * Checking binary requirements
-   * (could defined in a separate function)
-   */
-  if (bin_input_param == NULL || bin_output_param == NULL)
-  {
-    dprintf(STDERR, "Bad usage! See HELP [--help|-h]\n");
+    /* Open file1 for reading */
+    fd1 = open(argv[1], O_RDONLY);
+    if (fd1 == -1) {
+        perror("open");
+        exit(1);
+    }
 
-    // Freeing allocated data
-    free_if_needed(bin_input_param);
-    free_if_needed(bin_output_param);
-    // Exiting with a failure ERROR CODE (== 1)
-    exit(EXIT_FAILURE);
-  }
+    /* Create file2 */
+    fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+    if (fd2 == -1) {
+        perror("open");
+        close(fd1);
+        exit(1);
+    }
 
-  // Printing params
-  dprintf(1, "** PARAMS **\n%-8s: %s\n%-8s: %s\n%-8s: %d\n",
-          "input", bin_input_param,
-          "output", bin_output_param,
-          "verbose", is_verbose_mode);
+    /* Copy the contents of file1 to file2 */
+    while ((nread = read(fd1, buffer, sizeof buffer)) > 0) {
+        if (write(fd2, buffer, nread) != nread) {
+            perror("write");
+            close(fd1);
+            close(fd2);
+            exit(1);
+        }
+    }
 
-  // Business logic must be implemented at this point
+    /* Close both files */
+    close(fd1);
+    close(fd2);
 
-  
-
-  // Freeing allocated data
-  free_if_needed(bin_input_param);
-  free_if_needed(bin_output_param);
-
-  return EXIT_SUCCESS;
+    return 0;
 }
